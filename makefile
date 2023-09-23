b30ee031347cc07135e0e9ee9e1fa3e34dc35203
Tj Campanella <tj@arete.health>
2023-09-22 21:20:12 -0400

    Solve aoc1

--- makefile
+++ makefile
@@ -14,7 +14,7 @@ tests: FORCE
 	cargo test
 
 lint: FORCE
-	cargo clippy --all-targets --color always -- -D warnings -D clippy::pedantic -D clippy::nursery -D clippy::unwrap_used -D clippy::expect_used
+	cargo clippy --all-targets --color always --allow-dirty --allow-staged --fix -- -D warnings -D clippy::pedantic -D clippy::nursery -D clippy::unwrap_used -D clippy::expect_used
 
 build: FORCE lint tests
 	cargo build
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn aoc1() -> Option<u32> {
+    let contents = fs::read_to_string("input.txt").map_or_else(
+        |_| Vec::new(),
+        |contents| {
+            contents
+                .split(char::is_whitespace)
+                .map(str::parse::<u32>)
+                .collect::<Vec<_>>()
+        },
+    );
+
+    let mut results: Vec<u32> = vec![];
+    let mut temp = 0;
+    for val in contents {
+        if let Ok(val) = val {
+            temp += val;
+        } else {
+            results.push(temp);
+            temp = 0;
+        }
+    }
+
+    return results.iter().max().copied();
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::aoc1;
+
+    #[test]
+    fn aoc1_test() {
+        assert_eq!(Option::Some(68467), aoc1());
+    }
 }
