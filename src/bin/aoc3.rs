b0f36891aaaa47578bf7cad4efdb34e2771a0a64
Tj Campanella <tj@arete.health>
2023-10-11 21:11:45 -0400

    Setup aoc3

---
+++ src/bin/aoc3.rs
@@ -0,0 +1,15 @@
+const fn main() {}
+
+const fn aoc3p1() -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::aoc3p1;
+
+    #[test]
+    fn aoc3p1_test() {
+        assert_eq!(0, aoc3p1());
+    }
+}
